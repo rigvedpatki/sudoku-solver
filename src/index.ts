b4a72557657e6a7aad0e85100faf098a7b517a2e
Rigved Patki <rigvedp@ecozensolutions.com>
2020-07-15 18:55:50 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+async function main() {
+  try {
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+console.log('Execution started')
+main().then(() => {
+  console.log('Execution finished')
+})
